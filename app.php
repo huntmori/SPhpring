8200ae644cf2839ed34cb935ddf405aca2097c38
huntmori <huntmori04x22@gmail.com>
2022-08-22 14:52:31 +0000

    build test

--- app.php
+++ app.php
@@ -1,18 +1,19 @@
 <?php  
-require __DIR__."/vendor/autoload.php";
+// require "vendor/autoload.php";
 
-use React\Http\HttpServer as HttpServer;
-use React\Socket\SocketServer as SocketServer;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use React\Http\Message\Response as Response;
+// use React\Http\HttpServer as HttpServer;
+// use React\Socket\SocketServer as SocketServer;
+// use Psr\Http\Message\ServerRequestInterface as Request;
+// use React\Http\Message\Response as Response;
 
-$http = new HttpServer(function (Request $request) {
-    return Response::plainText(
-        "Heelo World!\n"
-    );
-});
+// $http = new HttpServer(function (Request $request) {
+//     return Response::plainText(
+//         "Heelo World!\n"
+//     );
+// });
 
-$socket = new SocketServer('127.0.0.1:8080');
-$http->listen($socket);
+// $socket = new SocketServer('127.0.0.1:8080');
+// $http->listen($socket);
 
-echo "server running at http://127.0.0.1:8080".PHP_EOL;
+// echo "server running at http://127.0.0.1:8080".PHP_EOL;
+echo "hello world";
