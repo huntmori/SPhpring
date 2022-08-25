1db591618148d83692d88d6fdcaafb36341b3309
huntmori <huntmori04x22@gmail.com>
2022-08-26 00:12:06 +0900

    Class Recursive AttributeScan Test

--- Test.php
+++ Test.php
@@ -2,13 +2,32 @@
 require_once "vendor/autoload.php";
 
 use Sphpring\App\Index\Controller\IndexController;
-
+ini_set('memory_limit','-1');
 try {
     $controller = new IndexController();
 //    var_dump($controller);
-    $reflactor = new ReflectionClass($controller);
+    $reflactor = new ReflectionClass(IndexController::class);
     echo 'attribute';
     var_dump($reflactor->getAttributes()[0]->getName());
+
+    $test = factoryTest(IndexController::class, null);
+    var_dump($test);
+    var_dump($test->getProperties());
 } catch (ReflectionException $e) {
     var_dump($e->getTraceAsString());
+}
+
+function factoryTest($className, $instance): object{
+    var_dump($className);
+    $reflect = new ReflectionClass($className);
+    $instance = $reflect->newInstanceWithoutConstructor();
+    $attributes = $reflect->getAttributes();
+    foreach($attributes as $attribute) {
+        if($attribute->getName()!="Attribute") {
+            $reflect->
+            $instance->{$attribute->getName()} = $attribute->getArguments();
+            return factoryTest($attribute->getName(), $instance);
+        }
+    }
+    return $instance;
 }
